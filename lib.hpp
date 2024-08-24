7b5554b2024ddae123ebeeed07d5ba00331376fb
Fortex1 <mykolademan@gmail.com>
2024-08-24 15:07:27 +0300

    Add files via upload

---
+++ lib.hpp
@@ -0,0 +1,10 @@
+#ifndef LIB_HPP
+#define LIB_HPP
+#include <iostream>
+void print(const char str[]);
+template <typename T>
+void input(T &value);
+void print(int num);
+void print(double num);
+
+#endif //LIB_HPP
---
+++ libfn.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "lib.hpp"
+using namespace std;
+void print(const char str[]){
+    cout<<str;
+    
+}
+template <typename T>
+void input(T &value){
+    cin>>value;
+}
+void print(int num){
+    cout<<num;
+}
+void print(double num2){
+    cout<<num2;
+}
